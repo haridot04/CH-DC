442c2cb483f8f9ec891dec55a51d95de84c8d359
Harikrishnan R U <hari@Harikrishnans-MacBook-Pro.local>
2021-01-23 08:34:07 +0530

    first commit

--- app.js
+++ app.js
@@ -1,10 +1,8 @@
 const main = document.querySelector(".main");
 console.log("done");
 const t1 = new TimelineLite ({paused: true})
-console.log(t1);
-t1.to('.bg', 2, {
-    transform: "scale(1.2)",
-    ease: Power2.easeOut
-});
-
-t1.play()
+// t1.to('.bg', 2, {
+//     transform: "scale(1.2)",
+//     ease: Power2.easeOut
+// })
+// t1.play()
